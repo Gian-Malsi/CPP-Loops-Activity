80924e864f6c5cf2b8be8dfc4aa56d1b6c8caef3
Gian-Malsi <giancarlomalsi0@gmail.com>
2026-05-25 13:32:21 +0800

    Update CPP Loops Activity.cpp

--- CPP Loops Activity.cpp
+++ CPP Loops Activity.cpp
@@ -5,6 +5,6 @@ int main(){
 int n;
 cout<<"Enter a number to start countdown: ";
 cin>>n;
-if(!n>0){cout<<"Invalid Input! Please enter a number greater than 0.";return 0;}
+if(n<=0){cout<<"Invalid Input! Please enter a number greater than 0.";return 0;}
 for(;n;){cout<<n--<<" ";}
 return 0;}
